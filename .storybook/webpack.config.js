57901af402d9d7b953409d1b77a9e6c370d650fd
whitecalm <whitecalm@outlook.com>
2019-09-08 11:09:33 +0300

    no message

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", 
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+}
---
+++ src/.stories/SortableList.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="list">
+    <slot />
+  </ul>
+</template>
+
+<script>
+import { ContainerMixin } from '../index';
+
+export default {
+  mixins: [ContainerMixin]
+}
+</script>
